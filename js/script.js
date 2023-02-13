347bb7ecca4df7f588800bb71e2231c14adab062
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-02-13 13:32:44 +0600

    project complete

--- js/script.js
+++ js/script.js
@@ -22,9 +22,26 @@ document.getElementById("cards").addEventListener("click", (event) => {
         }
     }
 });
+//*player cost Button
+document.getElementById('calculate-player-cost').addEventListener('click', function () {
+    let perPlayerCost = document.getElementById('player-input').value;
+    let numberOfPlayer = document.getElementById('player-list').children.length;
+    if (perPlayerCost == '') {
+        alert("Enter the cost of a player");
+    }
+    else if (numberOfPlayer == 0) {
+        alert("Insert your favorite player name");
+    }
+    else {
+        let totalCostplayer = parseInt(perPlayerCost) * parseInt(numberOfPlayer);
+        document.getElementById('dollar-sign-player').style.display = "block";
+        document.getElementById("players-cost").innerText = totalCostplayer;
+    }
+
+})
 
 
-//*manager & coach
+//*total cost,manager & coach
 document.getElementById('calculate-total-cost').addEventListener('click', function () {
     //*manager section
     const managerField = document.getElementById('manager-input');
@@ -35,5 +52,25 @@ document.getElementById('calculate-total-cost').addEventListener('click', functi
     const coachField = document.getElementById('coach-input');
     const coachValueString = coachField.value;
     const coachValue = parseInt(coachValueString);
-    // console.log(coachValue);
+
+    //*player cost section
+    const playercostString = document.getElementById("players-cost").innerText;
+    const playerCost = parseInt(playercostString);
+
+    const totalCostField = document.getElementById('total-cost');
+
+    if (isNaN(coachValue) || isNaN(managerValue)) {
+        alert("Please,fill up the empty input value");
+    } else if (isNaN(playerCost)) {
+        alert("Please,Calculate the cost for players");
+    }
+
+    const totalCost = playerCost + coachValue + managerValue;
+
+
+    document.getElementById('dollar-sign-total').style.display = "block";
+    totalCostField.innerText = totalCost;
+
+
+
 })
